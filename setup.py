044b8df51b417dca1c62be9b5fe9d3ac3db63311
Raphael Jasjukaitis <r.jasjukaitis@raphaa.de>
2013-05-11 20:16:33 +0200

    Added localization.

--- setup.py
+++ setup.py
@@ -4,9 +4,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='cmsplugin-socialbuttons',
-    version='0.1',
+    version='0.1.1',
     description='A simple plugin for adding social buttons.',
-    long_description=open(os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
+    long_description=open(os.path.join(os.path.dirname(__file__),
+                                       'README.rst')).read(),
     author='Raphael Jasjukaitis',
     author_email='webmaster@raphaa.de',
     url='https://github.com/jurheinsieg/cmsplugin-socialbuttons',
